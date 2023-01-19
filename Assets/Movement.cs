ecbabcfd4d62188accb1644a9fa8660dbf5ae3c2
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2023-01-19 14:04:18 -0600

    deceleration

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,36 +6,59 @@ using System;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private Transform ship;
-    [SerializeField] private float speed = .5f;
-    private KeyCode pressedButton;
+    [SerializeField] private float maxSpeed = .5f;
+    private float speed;
+    private Vector3 moveDirection;
+    [SerializeField] private float slowDownAmount = .001f;
+    [SerializeField] private float speedUpAmount = .001f;
+    [SerializeField] private float moveAdjustmentAmount = .001f;
 
+    private void Start()
+    {
+        moveDirection = ship.forward;
+        speed = 0;
+    }
     void Update()
     {
+        SlowDown();
         Thrust();
+        Move();
         RotatLeft();
         RotatRight();
+        print(speed);
     }
 
     private void RotatLeft()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            ship.Rotate(0, -1, 0);
+            ship.Rotate(Vector3.left);
         }
     }
     private void RotatRight()
     {
         if (Input.GetKey(KeyCode.D))
         {
-            ship.Rotate(0, 1, 0);
+            ship.Rotate(Vector3.right);
         }
+    }
+
+    private void SlowDown()
+    {
+        speed = Mathf.Lerp(speed, 0, slowDownAmount);
     }
 
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            ship.position += ship.forward * speed * Time.deltaTime;
+            moveDirection = Vector3.Lerp(moveDirection, ship.forward, moveAdjustmentAmount);
+            speed = Mathf.Lerp(speed, maxSpeed, speedUpAmount);
         }
+    }
+
+    private void Move()
+    {
+        ship.position += moveDirection * speed * Time.deltaTime;
     }
 }
